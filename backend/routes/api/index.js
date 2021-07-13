f4cf78633b7a434e167e03bf73e746bd9b6a3c64
Shehan Atukorala <shehanatuk@gmail.com>
2021-07-12 23:37:39 -0400

    cleaned up redundant code

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,18 +1,18 @@
 var router = require('express').Router();
 var runIntent = require("./dialogflow").runIntent;
 
+// /api/requestText POST 
 router.post("/requestText", function(req, res){
-    // var intentRequest = createSessionPath(req.params.projectId);
-    // console.log(req.params.projectId);
-    (async() => { 
+    (async() => {
         var result = await runIntent(req.body.projectId, req.body.requestText);
         return res.send(
             {
                 "responseMessage": result.Response,
-                "originalQuery": result.Query,
+                "originalQuery": result.Query, 
                 "intent": result.intent
-            });
-    })()   
-});
+            }
+        )
+    })
+})
 
 module.exports = router;
